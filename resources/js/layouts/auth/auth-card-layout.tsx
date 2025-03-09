abb38c8695205a3d8d669c8437c06bfd8dc9d53f
Lukay Červenka <lukay.cervenka@gmail.com>
2025-03-09 14:55:59 +0100

    refactor(ui): Change app logo in login / register forms
    - Changed App logo for a new one in login and register forms

--- resources/js/layouts/auth/auth-card-layout.tsx
+++ resources/js/layouts/auth/auth-card-layout.tsx
@@ -1,7 +1,7 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
+import logo from '../../../../public/images/logo-small.png';
 
 export default function AuthCardLayout({
     children,
@@ -16,8 +16,8 @@ export default function AuthCardLayout({
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-md flex-col gap-6">
                 <Link href={route('home')} className="flex items-center gap-2 self-center font-medium">
-                    <div className="flex h-9 w-9 items-center justify-center">
-                        <AppLogoIcon className="size-9 fill-current text-black dark:text-white" />
+                    <div className="flex items-center justify-center">
+                        <img src={logo} alt={'File vault logo'} className="h-12" />
                     </div>
                 </Link>
 
